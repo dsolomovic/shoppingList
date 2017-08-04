65f2aaf259b34360f6ae50eb7da432690cf7e50a
David Solomovic <davidsolomovic@Davids-MacBook-Pro.local>
2017-08-04 13:47:31 +1000

    initial commit

---
+++ shoppinglist.rb
@@ -0,0 +1,26 @@
+list = []
+
+#my shopping list
+puts " welcome to my shopping list"
+puts "would you like to continue? [y]es or [n]o"
+answer= gets.chomp.downcase
+
+if answer == "n"
+print "bye"
+else
+puts "to exit the program type [q]\n"
+
+run = 1
+while run == 1
+puts "add items to shoppinglist:" 
+item = gets.chomp
+if item == "q"
+run = 0
+print " this is you list\n "
+puts list
+else
+list.push (item)
+
+end
+end
+end
