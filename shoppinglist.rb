4c05ba8f3b160ca25c63d59f381b8fbfb9ee1894
David Solomovic <davidsolomovic@Davids-MacBook-Pro.local>
2017-08-04 14:48:26 +1000

    updated rb

--- shoppinglist.rb
+++ shoppinglist.rb
@@ -12,14 +12,14 @@ puts "to exit the program type [q]\n"
 
 run = 1
 while run == 1
-puts "add items to shoppinglist:" 
+puts "add items to shoppinglist:"
 item = gets.chomp
 if item == "q"
 run = 0
 print " this is you list\n "
 puts list
-else
-list.push (item)
+ else
+ list.push (item)
 
 end
 end
